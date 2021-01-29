81c34d0e26028d7d5d5ab4099c32bd334bcefed2
dr-chene <3286586983@qq.com>
2021-01-29 15:40:38 +0800

    change some annotations

--- app/src/main/java/com/dr_chene/mvvmdemo/repository/ArticleRepository.java
+++ app/src/main/java/com/dr_chene/mvvmdemo/repository/ArticleRepository.java
@@ -38,7 +38,7 @@ public class ArticleRepository {
 
     //加载数据，思路是通过网络加载数据，如果失败则从本地加载数据
     //事实上我觉得这样的模式会消耗大量的流量，并且在网络优良的情况下本地room数据基本没用
-    //我是考虑到用户既然要使用该app，就是想获得最新的数据，所以才使用了这样一个模式
+    //我是考虑到用户既然要使用该app，就是想获得最新的数据(网上)，所以才使用了这样一个模式
     private List<Article> getArticleByPage(int page) {
         if (over) {
             Toast.makeText(App.getContext(), "no more data", Toast.LENGTH_SHORT).show();
@@ -56,7 +56,7 @@ public class ArticleRepository {
 //        articles = null;
         if (articles == null) {
             //这有一个bug，app初始化就从room加载本地数据会显示找不到，成功显示ui后就不会
-            //可以将PageArticle中的Maybe改为Single看看这个bug
+            //可以将PageArticleDao中的Maybe改为Single看看这个bug
             articles = pageArticleDao.getArticleByPage(page)
                     .subscribeOn(io.reactivex.schedulers.Schedulers.io())
                     .blockingGet();
